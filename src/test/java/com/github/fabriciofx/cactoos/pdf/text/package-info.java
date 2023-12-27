ddff51fe02e09cfb2dbd73c11f22a654475132fa
Fabrício Barros Cabral <fabriciofx@gmail.com>
2023-12-27 10:40:00 -0300

    (#64) Move DateTest and EscapedTest to text package

--- src/test/java/com/github/fabriciofx/cactoos/pdf/text/package-info.java
+++ src/test/java/com/github/fabriciofx/cactoos/pdf/text/package-info.java
@@ -21,25 +21,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.github.fabriciofx.cactoos.pdf;
-
-import com.github.fabriciofx.cactoos.pdf.text.Date;
-import org.junit.jupiter.api.Test;
-import org.llorllale.cactoos.matchers.Assertion;
-import org.llorllale.cactoos.matchers.IsText;
 
 /**
- * Test case for {@link Date}.
+ * Tests for Text.
  *
  * @since 0.0.1
  */
-final class DateTest {
-    @Test
-    void show() throws Exception {
-        new Assertion<>(
-            "Must represent a PDF date",
-            new Date(2023, 12, 11, 20, 11, 32, "Etc/GMT-3"),
-            new IsText("D:20231211201132+03'00'")
-        ).affirm();
-    }
-}
+package com.github.fabriciofx.cactoos.pdf.text;
